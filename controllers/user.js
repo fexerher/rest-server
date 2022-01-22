285f10153c8aa1d3281c44317674c021df411700
Andrei <dyausinoh@gmail.com>
2022-01-22 16:54:47 +0100

    Rest Server

--- controllers/user.js
+++ controllers/user.js
@@ -2,13 +2,23 @@ const { response, request  } = require('express')
 const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 
- const usuariosGet = (req = request, res = response)  => {
-     const {q, nombre = 'no Name', apikey} = req.query;
+ const usuariosGet =  async (req = request, res = response)  => {
+
+
+     //const {q, nombre = 'no Name', apikey} = req.query;
+     const { limite = 5, desde = 0} = req.query;
+     const query = {estado:  true};
+
+    const [total, usuarios] = await  Promise.all([
+        Usuario.countDocuments(query),
+        Usuario.find(query)
+          .skip(Number( desde ))
+          .limit( Number( limite ) )
+    ])
+
     res.json({
-        msg: 'get Api - controlador',
-        q,
-        nombre,
-        apikey
+        total,
+        usuarios
     })
 }
 const usuariosPut = async (req, res)  =>  {
@@ -23,10 +33,7 @@ const usuariosPut = async (req, res)  =>  {
     }
     const usuario = await Usuario.findByIdAndUpdate( id, resto )
 
-    res.json({
-        msg: 'put Api - controlador',
-        usuario
-    })
+    res.json(usuario)
 }
 const usuariosPost = async (req, res)  => {
 
@@ -52,10 +59,15 @@ const usuariosPatch = (req, res)  => {
         msg: 'patch Api - controlador'
     })
 }
-const usuariosDelete = (req, res)  => {
-    res.json({
-        msg: 'delete Api - controlador'
-    })
+const usuariosDelete = async (req, res)  => {
+
+    const {id}  = req.params
+
+    //Fisicamente lo borramos
+    //const usuario = await Usuario.findByIdAndDelete( id )
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false } )
+
+    res.json(usuario)
 }
 
 
