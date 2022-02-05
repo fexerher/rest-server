e6e2b4ed384f0caf85602c545a433f9b783df20f
Andrei <dyausinoh@gmail.com>
2022-02-05 21:11:28 +0100

    Error crashed resolved

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor(){
         this.app =  express()
-        this.port = process.env.PORT || 7976
+        this.port = process.env.PORT
 
         this.paths = {
             auth:       '/api/auth',
